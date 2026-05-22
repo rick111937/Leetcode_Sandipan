59b08933d2494f4c67679ef542b59e3ffed85027
Sandipan Mondal <sukhenmandal836@gmail.com>
2026-05-22 12:21:59 +0530

    Add files via upload

---
+++ 20.05.2026- Code 14(Permutations).py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def backtrack(path, remaining):
+            if not remaining:
+                result.append(path)
+                return
+            for i in range(len(remaining)):
+                backtrack(path + [remaining[i]],
+                          remaining[:i] + remaining[i+1:])
+        backtrack([], nums)
+        return result
