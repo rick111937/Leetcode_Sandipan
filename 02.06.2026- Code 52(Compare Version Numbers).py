ae50ee57421cf65d91d8a10212da416caa9643d3
Sandipan Mondal <sukhenmandal836@gmail.com>
2026-06-11 00:35:46 +0530

    Add files via upload

---
+++ 02.06.2026- Code 52(Compare Version Numbers).py
@@ -0,0 +1,13 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1 = list(map(int, version1.split('.')))
+        v2 = list(map(int, version2.split('.')))
+        max_len = max(len(v1), len(v2))
+        v1 += [0] * (max_len - len(v1))
+        v2 += [0] * (max_len - len(v2))
+        for i in range(max_len):
+            if v1[i] > v2[i]:
+                return 1
+            elif v1[i] < v2[i]:
+                return -1
+        return 0
